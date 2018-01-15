1c50bcdf1ddce316340fc27e0967c3f5af0c651f
Santiago Roca <snroca@hotmail.com>
2018-01-15 15:13:11 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: {
+        lib: ['./index.js']
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'fastsvgviewer.js'
+        library: 'FastSvgViewer',
+        publicPath: 'http://localhost:8080/built'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ],
+
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
